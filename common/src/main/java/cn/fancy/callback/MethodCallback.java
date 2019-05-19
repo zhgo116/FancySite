959d0c67f8a19529839c78c45c69741be3c2beba
zhgo116 <zhgo116@qq.com>
2019-05-19 22:15:02 +0800

    修改项目模式i123

---
+++ common/src/main/java/cn/fancy/callback/MethodCallback.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
+ */
+package cn.fancy.callback;
+
+/**
+ * 方法回调接口
+ * @author ThinkGem
+ */
+public interface MethodCallback {
+
+	Object execute(Object... params);
+	
+}
--- fancyutil/src/main/java/cn/fancy/utils/LogType.java
+++
@@ -1,5 +0,0 @@
-package cn.fancy.utils;
-
-public class LogType {
-	public static final String LOGIN = "1";
-}
